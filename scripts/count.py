2f8961c4e32f45151ddfeeed6427a1cf106d05c6
eric-ishibashi <77333121+eric-ishibashi@users.noreply.github.com>
2021-01-17 00:42:44 +0900

    Update and rename count.py to scripts/count.py

--- scripts/count.py
+++ scripts/count.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
-
 rospy.init_node('count')                                # ノード名「count」に設定
 pub = rospy.Publisher('count_up', Int32, queue_size=1)  # パブリッシャ「count_up」を作成
 rate = rospy.Rate(10)                                   # 10Hzで実行
