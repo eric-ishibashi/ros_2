7c11586b94d2f2277a7a2673dfaf71891e781c2b
eric-ishibashi <77333121+eric-ishibashi@users.noreply.github.com>
2021-01-20 16:54:49 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -4,9 +4,11 @@ from std_msgs.msg import Int32
 
 rospy.init_node('count')                                # ノード名「count」に設定
 pub = rospy.Publisher('count_up', Int32, queue_size=1)  # パブリッシャ「count_up」を作成
-rate = rospy.Rate(10)                                   # 10Hzで実行
+rate = rospy.Rate(1)                                    #1Hzで実行
 n = 0
 while not rospy.is_shutdown():
-    n += 1
+    print("入力された数字が足されます。")
+    a = int(input())
+    n += a
     pub.publish(n)
     rate.sleep()
